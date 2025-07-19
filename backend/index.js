4bcd56539166e9ac5ccc180aa19dca7e54694fca
pranavyedla <pranavyedla2004@gmail.com>
2025-07-19 22:34:23 +0530

    Restore

--- backend/index.js
+++ backend/index.js
@@ -12,14 +12,7 @@ app.get("/", (req, res) => {
 });
 // middlewares
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173", // allow Vite dev server during development
-    credentials: true,
-  })
-);
-app.use(express.urlencoded({ extended: true }));
-// File upload middleware 
+app.use(cors());
 // API ENDPOINTS
 app.use("/images", express.static("uploads"));
 app.use("/user", userRoutes);
